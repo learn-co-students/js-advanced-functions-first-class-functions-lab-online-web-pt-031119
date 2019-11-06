c5e46ed268e71e5f2db13d44684bdb80bb277cd0
thistonyb <this.tonybaker@gmail.com>
2019-11-06 13:48:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(driverArray) {
+  return driverArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(driverArray) {
+  return driverArray.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(someValue) {
+  return function(fare) {
+    return fare * someValue;
+  };
+}
+
+const fareDoubler = function(fare) {
+  const multiplyByTwo = createFareMultiplier(2);
+  return multiplyByTwo(fare);
+};
+/**
+ * Above and below are basically the same.
+ * Above is redundant in that it is assigned an anonymous function. createFareMultiplier already does that.
+ */
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverArray, driverSelector) {
+  return driverSelector(driverArray);
+}
